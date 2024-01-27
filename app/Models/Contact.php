e692db00b0829f1dc94ae1f94eecf754da4b2897
MarcosAparicio <marcos.aparicio593@gmail.com>
2024-01-27 15:11:49 -0500

    adding migrations for the contacts table(and the model too)

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
+    protected $fillable = [
+        'name',
+        'email',
+        'phone_number',
+        'dob',
+        'user_id'
+    ];
+
     use HasFactory;
 }
