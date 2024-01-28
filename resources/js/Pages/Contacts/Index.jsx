14fd91ea4aab84cf912ef97c929e6d47fb3bd41d
MarcosAparicio <marcos.aparicio593@gmail.com>
2024-01-28 11:20:08 -0500

    adding the flash message component in the contacts index page

--- resources/js/Pages/Contacts/Index.jsx
+++ resources/js/Pages/Contacts/Index.jsx
@@ -1,8 +1,12 @@
 import { ContactCard } from "@/AppComponents/ContactCard";
+import { FlashMessage } from "@/AppComponents/FlashMessage";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, usePage } from "@inertiajs/react";
 
 export default function Index({ auth, contacts }) {
+    const { flash } = usePage().props;
+    console.log("flash: ", flash);
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -14,9 +18,20 @@ export default function Index({ auth, contacts }) {
         >
             <Head title="Home" />
 
-            <h1 className="py-12 text-center text-4xl font-bold">
-                Your Contacts
-            </h1>
+            <div className="py-10 ">
+                <h1 className="my-3 text-center text-4xl font-bold">
+                    Your Contacts
+                </h1>
+
+                {flash.message ? (
+                    <FlashMessage
+                        message={flash?.message}
+                        type={flash?.type}
+                        key={flash?.id}
+                    />
+                ) : null}
+            </div>
+
             <div className="grid-flow-rows mx-auto grid w-3/4 grid-cols-1 justify-between justify-items-center gap-14 pb-6 sm:grid-cols-3 md:grid-cols-3 xl:grid-cols-5">
                 {contacts.length < 1 ? (
                     <div className="col-span-full flex flex-col justify-center self-center justify-self-center text-center">
@@ -29,7 +44,9 @@ export default function Index({ auth, contacts }) {
                         </a>
                     </div>
                 ) : (
-                    contacts.map((contact) => <ContactCard contact={contact} />)
+                    contacts.map((contact) => (
+                        <ContactCard key={contact.id} contact={contact} />
+                    ))
                 )}
             </div>
         </AuthenticatedLayout>
