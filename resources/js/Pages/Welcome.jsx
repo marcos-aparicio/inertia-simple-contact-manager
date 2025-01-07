221b0f717c91199c22b9b906f03500ea0b85cad8
marcos-aparicio <marcos.aparicio593@gmail.com>
2025-01-07 17:49:31 -0500

    fixing bg-cover in the welcome page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -8,7 +8,7 @@ export default function Welcome({ auth }) {
             <Head title="Welcome" />
             <div className="flex h-screen flex-col bg-gray-100 bg-center selection:bg-red-500 selection:text-white dark:bg-gray-900 dark:text-white sm:flex sm:items-center sm:justify-center">
                 <WelcomeHeader auth={auth} />
-                <div className="flex w-full grow flex-col items-center justify-center gap-5 bg-[url('../img/light_background.jpg')] px-2 text-center sm:gap-3">
+                <div className="flex w-full grow flex-col items-center justify-center gap-5 bg-[url('../img/light_background.jpg')] px-2 text-center sm:gap-3 bg-cover">
                     <h1 className="text-4xl font-bold sm:text-6xl">
                         {appName ?? "Contact Manager"}
                     </h1>
